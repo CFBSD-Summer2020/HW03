ac2da85f6bad0fc8a1c322a50817cf4f906b1516
caichufan <sunnycaibaobao@gmail.com>
2020-07-21 00:26:13 -0500

    life expectance

---
+++ Code.R
@@ -0,0 +1,29 @@
+# The data I am working on is about the life expectancy from all contries from year 1800 to 2100, [Behavioral Risk Factor Surveillance System (BRFSS)](https://www.cdc.gov/brfss/about/index.htm)
+library(tidyverse)
+library(ggplot2)
+
+# modify the dataset structure
+life <- read.csv("life_expectancy_years.csv", header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
+rownames(life) <- life[,1]
+life$country <- NULL
+
+# Then, I would like to look at the life expactancy trend for China from year 1800 to 2100
+year <- colnames(life)
+year <- as.numeric(year)
+China <- as.numeric(life[c("China"),])
+
+# Then, I would like to look at the life expactancy trend for United States from year 1800 to 2100
+year <- colnames(life)
+year <- as.numeric(year)
+USA <- as.numeric(life[c("United States"),])
+
+#draw the plot
+new_life <- data.frame(year, China, USA)
+colors <- c("China" = "red", "USA" = "blue")
+
+ggplot(new_life)+
+  geom_point(aes(x = year, y = China, color = "China"))+
+  geom_point(aes(x = year, y = USA, color = "USA"))+
+  labs(title = "Life Expectancy change", x = "Year", y = "Life Expectancy", color = "Legend")+
+  scale_color_manual(values = colors)
+
